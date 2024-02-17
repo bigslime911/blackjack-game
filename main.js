ced6ac4a4073a1f2842248e723ab5182a3b3a8fc
bigslime892001 <donkorfrancis1000@icloud.com>
2024-02-17 01:18:14 +0000

    a normal game

---
+++ main.js
@@ -0,0 +1,31 @@
+let firstCard = 10;
+let secondCard = 12;
+let sum = firstCard + secondCard ;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+
+
+
+
+function startEl(){
+    if (sum <= 20){
+        message = "Do yo ant to draw a new card?";
+    } else if (sum === 21){
+        message = "woohoo, You've Blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game.";
+        isAlive = false;
+    }
+    
+    document.getElementById("message-el").textContent = message;
+    document.getElementById("card-el").textContent = `Cards: ${firstCard} ${secondCard}`;
+    document.getElementById("sum-el").textContent = `Sum: ${sum}`;
+    
+    
+}
+
+function newCard(){
+    document.querySelector("body").textContent= "Drawing a new card from the deck"
+}
